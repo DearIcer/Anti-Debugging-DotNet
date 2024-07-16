e92e488ff59119b7856328cb06e3a58c900a8b5c
DearIce <56225620+DearIcer@users.noreply.github.com>
2024-07-16 14:50:50 +0800

    commit

---
+++ Debugger.Launch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+while (true)
+{
+    try
+    {
+        // Debugger.Launch会尝试启动调试器，如果用户已经附加了调试器，它将不执行任何操作。
+        Debugger.Launch();
+        Console.WriteLine("No debugger attached.");
+    }
+    catch (System.ComponentModel.Win32Exception)
+    {
+        // 如果调试器已经附加，捕获Win32异常
+        Console.WriteLine("Debugger is attached.");
+    }
+}
---
+++ FindWindows/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("user32.dll", CharSet = CharSet.Auto)]
+static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+string windowName = "x64dbg";
+
+while (true)
+{
+    if (FindWindow(null, windowName) != IntPtr.Zero)
+    {
+        Console.WriteLine("Debugger found.");
+    }
+    else
+    {
+        Console.WriteLine("No debugger found.");
+    }
+}
